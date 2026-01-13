30cf5f2d008d4fb2cf350b9f9f955a7deae4480e
Cristina Florea <crisflorea21@gmail.com>
2026-01-13 15:41:07 +0200

    Initial Commit

---
+++ LocalGuide/LocalGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalGuideApp.swift
+//  LocalGuide
+//
+//  Created by Cristina Florea on 13.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct LocalGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocalGuideTests/LocalGuideTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocalGuideTests.swift
+//  LocalGuideTests
+//
+//  Created by Cristina Florea on 13.01.2026.
+//
+
+import Testing
+@testable import LocalGuide
+
+struct LocalGuideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
